88c4a466251b94d83063d307431c1ea763c67c03
Lampion <saviopio03@gmail.com>
2025-06-06 11:37:30 +0700

    Add files via upload

---
+++ anykernel.sh
@@ -0,0 +1,33 @@
+### AnyKernel3 Ramdisk Mod Script
+## osm0sis @ xda-developers
+
+### AnyKernel setup
+# global properties
+properties() { '
+kernel.string=PoWeR v4.14.355 neutron-KSU by SaiKrishna for Redmi Note 8 Pro (begonia) | KernelSU Version: 10200
+do.devicecheck=1
+do.modules=0
+do.systemless=1
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=begonia
+device.name2=begoniain
+supported.versions=
+supported.patchlevels=
+supported.vendorpatchlevels=
+'; } # end properties
+
+# boot shell variables
+BLOCK=/dev/block/platform/bootdevice/by-name/boot;
+IS_SLOT_DEVICE=0;
+RAMDISK_COMPRESSION=auto;
+PATCH_VBMETA_FLAG=auto;
+
+# import functions/variables and setup patching - see for reference (DO NOT REMOVE)
+. tools/ak3-core.sh;
+
+# boot install
+dump_boot;
+
+write_boot;
+## end boot install
