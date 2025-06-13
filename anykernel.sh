edee8cffa339f4ff52a094e19fbe95d17809e242
Lampion <saviopio03@gmail.com>
2025-06-13 22:19:25 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Kurap1kaKernel For Redmi Note 9 By @vq6oon
+kernel.string=Lamp1onKernel For Redmi Note 9 By @vq6oon
 do.devicecheck=0
 do.modules=0
 do.systemless=1
