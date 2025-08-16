8ff40b49bf4b200e927cf5700952407c79c84959
Lampion <saviopio03@gmail.com>
2025-08-16 19:23:28 +0700

    Add files via upload

---
+++ anykernel.sh
@@ -0,0 +1,41 @@
+### AnyKernel3 Ramdisk Mod Script
+## osm0sis @ xda-developers
+
+### AnyKernel setup
+# global properties
+properties() { '
+kernel.string=
+do.devicecheck=1
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=dandelion
+device.name2=angelica
+device.name3=angelican
+device.name4=angelicain
+device.name5=cattail
+device.name6=blossom
+supported.versions=
+'; } # end properties
+
+
+### AnyKernel install
+## boot files attributes
+boot_attributes() {
+set_perm_recursive 0 0 755 644 $RAMDISK/*;
+set_perm_recursive 0 0 750 750 $RAMDISK/init* $RAMDISK/sbin;
+} # end attributes
+
+# boot shell variables
+BLOCK=/dev/block/by-name/boot;
+IS_SLOT_DEVICE=0;
+RAMDISK_COMPRESSION=auto;
+PATCH_VBMETA_FLAG=auto;
+
+# import functions/variables and setup patching - see for reference (DO NOT REMOVE)
+. tools/ak3-core.sh;
+
+# boot install
+dump_boot;
+
+write_boot;
+## end boot install
