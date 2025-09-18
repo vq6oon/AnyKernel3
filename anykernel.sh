f386fd0e82ef8fb24fa8dbb37efb3994aa54fe2d
vq6oon <saviopio03@gmail.com>
2025-09-18 17:59:36 +0700

    Aurora Stellar

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Lamp1onKernel For Redmi Note 8 Pro By @vq6oon
+kernel.string=AuroraStellar For Redmi Note 8 Pro By @vq6oon
 do.devicecheck=1
 do.modules=0
 do.systemless=1
